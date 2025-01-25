92ebac0be5266e3d0877845af6a90d789942ae27
Johann Ruiz <johannruiz176@vt.edu>
2025-01-25 16:17:41 -0500

    Project 0

--- GeoQuiz/app/src/main/java/edu/vt/cs5254/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/edu/vt/cs5254/geoquiz/MainActivity.kt
@@ -1,12 +1,23 @@
 package edu.vt.cs5254.geoquiz
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    // Name: Johann Ruiz
+    // Username: johannruiz176
+
+    private lateinit var trueButton: Button // lateinit means you'll set the value before you use it
+    private lateinit var falseButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +26,26 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener { view: View ->
+            Snackbar.make(
+                view,
+                R.string.correct_toast,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        falseButton.setOnClickListener { view: View ->
+            Snackbar.make(
+                this, // More control over the context used, can customize the theme
+                view,
+                getString(R.string.incorrect_toast),
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 }
