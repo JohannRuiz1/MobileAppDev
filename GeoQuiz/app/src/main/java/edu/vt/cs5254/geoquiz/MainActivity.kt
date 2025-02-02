f401d6e751d04a93e82c08de76e000c255c0cca2
Johann Ruiz <johannruiz176@vt.edu>
2025-02-01 21:11:13 -0500

    Pushing up project 1

--- GeoQuiz/app/src/main/java/edu/vt/cs5254/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/edu/vt/cs5254/geoquiz/MainActivity.kt
@@ -9,43 +9,66 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
+import edu.vt.cs5254.geoquiz.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     // Name: Johann Ruiz
     // Username: johannruiz176
 
-    private lateinit var trueButton: Button // lateinit means you'll set the value before you use it
-    private lateinit var falseButton: Button
+    private lateinit var binding: ActivityMainBinding
+
+    private val questionBank =  listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
-
-        trueButton.setOnClickListener { view: View ->
-            Snackbar.make(
-                view,
-                R.string.correct_toast,
-                Toast.LENGTH_SHORT
-            ).show()
+        binding.trueButton.setOnClickListener { view: View ->
+            checkAnswer(true, view)
         }
 
-        falseButton.setOnClickListener { view: View ->
-            Snackbar.make(
-                this, // More control over the context used, can customize the theme
-                view,
-                getString(R.string.incorrect_toast),
-                Toast.LENGTH_SHORT
-            ).show()
+        binding.falseButton.setOnClickListener { view: View ->
+            checkAnswer(false, view)
         }
+
+        binding.nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        updateQuestion()
+    }
+
+    private fun updateQuestion(){
+        val questionTextResId = questionBank[currentIndex].textResId
+        binding.questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean, view: View) {
+        val toast = if (userAnswer == questionBank[currentIndex].answer) R.string.correct_toast else R.string.incorrect_toast
+
+        Snackbar.make(
+            view,
+            toast,
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
---
+++ GeoQuiz/app/src/main/java/edu/vt/cs5254/geoquiz/Question.kt
@@ -0,0 +1,5 @@
+package edu.vt.cs5254.geoquiz
+
+import androidx.annotation.StringRes
+
+data class Question(@StringRes val textResId: Int, val answer: Boolean)
